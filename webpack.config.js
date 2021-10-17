12acfb5e75624a6afaa0f8738a912421803f96f1
lggg123 <machinelearner2334@gmail.com>
2021-10-17 16:02:03 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      path: __dirname + '/build',
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    devServer: {
+      contentBase: './build',
+    },
+    module: {
+      rules: [
+        {
+            test: /\.(png|jpg|gif|svg|jpe?g)$/i,
+            loader: 'file-loader',
+            options: {
+                publicPath: '/',
+                postTransformPublicPath: (p) => `__webpack_public_path__ + ${p}`,
+            },
+        },
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              "style-loader",
+              // Translates CSS into CommonJS
+              "css-loader",
+              // Compiles Sass to CSS
+              "sass-loader",
+            ],
+          },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+      ]
+    },
+  };

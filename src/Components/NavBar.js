6439e3ad8a12ced2565afcb4569d45408365f401
teqshark <teqpros@gmail.com>
2022-07-22 10:28:46 -0700

    made constant currentYear to pull Year automatically for copyright year

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,51 +1,50 @@
-import React from 'react'
-import avatar from '../img/avatar.png'
+import React from 'react';
+import avatar from '../img/avatar.png';
 import {NavLink} from 'react-router-dom';
 
 function NavBar() {
-    return (
-        <div className="NavBar">
-            <nav className="nav">
-                <div className="profile">
-                    <img src={avatar} alt=""/>
-                </div>
-
-                <ul className="nav-items">
-                    <li className="nav-item">
-                        <NavLink to="/" exact activeClassName="active">
-                            Home 
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/about" exact activeClassName="active">
-                            About
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/portfolios" exact activeClassName="active">
-                            Portfolios
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/blog" exact activeClassName="active">
-                            Blog
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/contact" exact activeClassName="active">
-                            Contact
-                        </NavLink>
-                    </li>
-                </ul>
-
-                <footer className="footer">
-                    <p>
-                        @2021 Copyright
-                    </p>
-                </footer>
-            </nav>
+  const currentYear = new Date().getFullYear();
+  return (
+    <div className="NavBar">
+      <nav className="nav">
+        <div className="profile">
+          <img src={avatar} alt="" />
         </div>
-    )
+
+        <ul className="nav-items">
+          <li className="nav-item">
+            <NavLink to="/" exact activeClassName="active">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/about" exact activeClassName="active">
+              About
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/portfolios" exact activeClassName="active">
+              Portfolios
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/blog" exact activeClassName="active">
+              Blog
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/contact" exact activeClassName="active">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+
+        <footer className="footer">
+          <p>@ {currentYear} Copyright</p>
+        </footer>
+      </nav>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
